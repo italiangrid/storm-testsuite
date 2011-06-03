a29ab20b2c79c18548f1a7a8a1aea605652bfb16
Elisabetta Ronchieri <elisabetta.ronchieri@cnaf.infn.it>
2011-06-03 17:14:56 +0000

    updated version

--- tstorm/__init__.py
+++ tstorm/__init__.py
@@ -1,18 +1,13 @@
 __author__ = 'Elisabetta Ronchieri'
 
-VERSION = (1, 0, 0, 'final', 0)
+VERSION = (1, 0, 1, 1)
 
 def get_version():
-    version = '%s.%s.%s' % (VERSION[0], VERSION[1], VERSION[2])
-    if VERSION[2]:
-        version = '%s.%s' % (version, VERSION[2])
-    if VERSION[3:] == ('alpha', 0):
-        version = '%s pre-alpha' % version
-    else:
-        if VERSION[3] != 'final':
-            version = '%s %s %s' % (version, VERSION[3], VERSION[4])
+    version = '%s.%s.%s-%s' % (VERSION[0], VERSION[1], VERSION[2], VERSION[3])
+    print version 
     from tstorm.utils.version import get_svn_revision
     svn_rev = get_svn_revision()
     if svn_rev != u'SVN-unknown':
         version = "%s %s" % (version, svn_rev)
+    print version
     return version
