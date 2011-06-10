adc06d4bbaec7af5b783449986e460fb43512943
Elisabetta Ronchieri <elisabetta.ronchieri@cnaf.infn.it>
2011-06-10 19:38:11 +0000

    updated

--- tstorm/test/regressiontest.py
+++ tstorm/test/regressiontest.py
@@ -8,6 +8,8 @@ from tstorm.utils import cp
 from tstorm.utils import rm
 from tstorm.utils import rmdir
 from tstorm.utils import cksm
+from tstorm.utils import space
+from tstorm.utils import sizefile
 
 class RegressionTest(unittest.TestCase):
     def __init__(self, testname, tfn, ifn, dfn,bifn):
@@ -16,28 +18,25 @@ class RegressionTest(unittest.TestCase):
       self.ifn = ifn
       self.dfn = dfn
       self.bifn = bifn
-    
-    def test_ls_bt(self):
-      self.lsbt_result = ls.LcgLs(self.tsets['general']['endpoint'], self.tsets['general']['accesspoint'], self.dfn).get_output()
-      self.assert_(self.lsbt_result['status'] == 'FAILURE')
 
-    def test_cp_bt(self):
-      self.cpbt_result = cp.LcgCp(self.tsets['general']['endpoint'], self.tsets['general']['accesspoint'], self.ifn, self.dfn, self.bifn).get_output()
-      self.assert_(self.cpbt_result['status'] == 'PASS')
-
-    def test_ls_at(self):
+    def test_ls_file(self):
       self.lsat_result = ls.LcgLs(self.tsets['general']['endpoint'], self.tsets['general']['accesspoint'], self.dfn).get_output()
       self.assert_(self.lsat_result['status'] == 'PASS')
       self.lcksm_result = cksm.CksmLf(self.ifn).get_output()
       self.assert_(self.lsat_result['Checksum'] == self.lcksm_result['Checksum'])
 
-    def test_rm(self):
-      self.rm_result = rm.SrmRm(self.tsets['general']['endpoint'], self.tsets['general']['accesspoint'], self.dfn).get_output()
-      self.assert_(self.rm_result['status'] == 'PASS')
-
-    def test_rmdir(self):
-      if '/' in self.dfn:
-        a=os.path.dirname(self.dfn)
-        self.rmdir_result = rmdir.SrmRmdir(self.tsets['general']['endpoint'], self.tsets['general']['accesspoint'], a).get_output()
-        for x in self.rmdir_result['status']:
-          self.assert_(x == 'PASS')
+    def test_update_used_soace_upon(self):
+      self.st_result = space.StoRMGst(self.tsets['general']['endpoint'], self.tsets['general']['accesspoint'], self.tsets['general']['spacetoken']).get_output()
+      self.assert_(self.st_result['status'] == 'PASS')
+      self.sm1_result = space.StoRMGsm(self.tsets['general']['endpoint'], self.tsets['general']['accesspoint'], self.st_result['arrayOfSpaceTokens']).get_output()
+      self.assert_(self.sm1_result['status'] == 'PASS')
+      self.ls_result = ls.LcgLs(self.tsets['general']['endpoint'], self.tsets['general']['accesspoint'], self.dfn).get_output()
+      self.assert_(self.ls_result['status'] == 'PASS')
+      self.cp_result = cp.LcgCp(self.tsets['general']['endpoint'], self.tsets['general']['accesspoint'], self.ifn, self.dfn, self.bifn).get_output()
+      print self.cp_result
+      self.assert_(self.cp_result['status'] == 'PASS')
+      self.lls_result = sizefile.Ls(self.ifn).get_output()
+      self.assert_(self.lls_result['status'] == 'PASS')
+      self.sm2_result = space.StoRMGsm(self.tsets['general']['endpoint'], self.tsets['general']['accesspoint'], self.st_result['arrayOfSpaceTokens']).get_output()
+      print self.lls_result['status'], self.sm1_result['unused'], self.sm2_result['unused'] 
+      self.assert_(inf(self.lls_result['size']) == int(self.sm1_result['unused']) - int(self.sm2_result['unused']))
---
+++ tstorm/utils/space.py
@@ -0,0 +1,97 @@
+__author__ = 'Elisabetta Ronchieri'
+
+import commands
+import os
+from tstorm.utils import utils
+
+class StoRMGst:
+  def __init__(self, endpoint, accesspoint, st_descr):
+    self.endpoint = endpoint
+    self.accesspoint = accesspoint
+    self.st_descr = st_descr
+    self.cmd = {
+      'name': 'clientSRM',
+      'rqst_protocol': 'httpg'}
+    self.otpt = {
+      'status':'',
+      'statusCode':[],
+      'explanation':[],
+      'arrayOfSpaceTokens': ''}
+
+  def get_command(self):
+    a = self.cmd['name'] + ' gst  -e ' + self.cmd['rqst_protocol'] + '://' + self.endpoint + ':8444/' + ' -d ' + self.st_descr
+    print a
+    return a
+
+  def run_command(self):
+    a=()
+    if utils.cmd_exist(self.cmd['name']):
+      a=commands.getstatusoutput(self.get_command())
+    return a
+
+  def get_output(self):
+    a=self.run_command()
+    if len(a) > 0 and a[0] == 0:
+      if 'SRM_SUCCESS' in a[1]:
+        for x in self.otpt:
+          if x == 'status':
+            self.otpt['status'] = 'PASS'
+          elif x == 'arrayOfSpaceTokens':
+            self.otpt['arrayOfSpaceTokens'] =a[1].split('arrayOfSpaceTokens')[1].split('"')[1] 
+          else:
+            y = a[1].split('\n')
+            for z in y:
+              if x in z:
+                self.otpt[x].append(z.split(x)[1].split('="')[1].split('"')[0])
+      else:
+        self.otpt['status'] = 'FAILURE'
+    else:
+      self.otpt['status'] = 'FAILURE'
+
+    return self.otpt
+
+class StoRMGsm:
+  def __init__(self, endpoint, accesspoint, st_id):
+    self.endpoint = endpoint
+    self.accesspoint = accesspoint
+    self.st_id = st_id
+    self.cmd = {
+      'name': 'clientSRM',
+      'rqst_protocol': 'httpg'}
+    self.otpt = {
+      'status':'',
+      'statusCode':[],
+      'explanation':[],
+      'unused': ''}
+
+  def get_command(self):
+    a = self.cmd['name'] + ' gsm -e ' + self.cmd['rqst_protocol'] + '://' + self.endpoint + ':8444/' + ' -s ' + self.st_id
+    print a
+    return a
+
+  def run_command(self):
+    a=()
+    if utils.cmd_exist(self.cmd['name']):
+      a=commands.getstatusoutput(self.get_command())
+    return a
+
+  def get_output(self):
+    a=self.run_command()
+    if len(a) > 0 and a[0] == 0:
+      if 'SRM_SUCCESS' in a[1]:
+        for x in self.otpt:
+          if x == 'status':
+            self.otpt['status'] = 'PASS'
+          elif x == 'unusedSize':
+            self.otpt['unusedSize'] =a[1].split('unusedSize')[1].split('"')[1]
+          else:
+            y = a[1].split('\n')
+            #for z in y:
+            #  if x in z:
+            #    self.otpt[x].append(z.split(x)[1].split('="')[1].split('"')[0])
+      else:
+        self.otpt['status'] = 'FAILURE'
+    else:
+      self.otpt['status'] = 'FAILURE'
+
+    return self.otpt
