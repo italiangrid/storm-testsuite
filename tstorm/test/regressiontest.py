969f3f39c66de555096317fe4dc7bc9ff5281013
Elisabetta Ronchieri <elisabetta.ronchieri@cnaf.infn.it>
2011-06-13 09:03:36 +0000

    updated regression tests

--- tstorm/test/regressiontest.py
+++ tstorm/test/regressiontest.py
@@ -36,7 +36,33 @@ class RegressionTest(unittest.TestCase):
         for x in self.rmdir_result['status']:
           self.assert_(x == 'PASS')
 
-    def test_update_used_soace_upon(self):
+    def test_update_used_soace_upon_rm(self):
+      self.ls_result = ls.LcgLs(self.tsets['general']['endpoint'], self.tsets['general']['accesspoint'], self.dfn).get_output()
+      self.assert_(self.ls_result['status'] == 'FAILURE')
+      self.cp_result = cp.LcgCp(self.tsets['general']['endpoint'], self.tsets['general']['accesspoint'], self.ifn, self.dfn, self.bifn).get_output()
+      self.assert_(self.cp_result['status'] == 'PASS')
+      self.st_result = space.StoRMGst(self.tsets['general']['endpoint'], self.tsets['general']['accesspoint'], self.tsets['general']['spacetoken']).get_output()
+      self.assert_(self.st_result['status'] == 'PASS')
+      self.sm1_result = space.StoRMGsm(self.tsets['general']['endpoint'], self.tsets['general']['accesspoint'], self.st_result['arrayOfSpaceTokens']).get_output()
+      self.assert_(self.sm1_result['status'] == 'PASS')
+      self.ls_result = ls.LcgLs(self.tsets['general']['endpoint'], self.tsets['general']['accesspoint'], self.dfn).get_output()
+      self.assert_(self.ls_result['status'] == 'PASS')
+      self.rm_result = rm.StoRMRm(self.tsets['general']['endpoint'], self.tsets['https']['voms'], self.dfn).get_output()
+      self.assert_(self.rm_result['status'] == 'PASS')
+      if '/' in self.dfn:
+        a=os.path.dirname(self.dfn)
+        self.rmdir_result = rmdir.StoRMRmdir(self.tsets['general']['endpoint'], self.tsets['general']['accesspoint'], a).get_output()
+        for x in self.rmdir_result['status']:
+          self.assert_(x == 'PASS')
+      self.ls_result = ls.LcgLs(self.tsets['general']['endpoint'], self.tsets['general']['accesspoint'], self.dfn).get_output()
+      self.assert_(self.ls_result['status'] == 'FAILURE')
+      self.lls_result = sizefile.Ls(self.ifn).get_output()
+      self.assert_(self.lls_result['status'] == 'PASS')
+      self.sm2_result = space.StoRMGsm(self.tsets['general']['endpoint'], self.tsets['general']['accesspoint'], self.st_result['arrayOfSpaceTokens']).get_output()
+      self.assert_(int(self.lls_result['size']) == int(self.sm2_result['unusedSize']) - int(self.sm1_result['unusedSize']))
+
+
+    def test_update_used_soace_upon_pd(self):
       self.st_result = space.StoRMGst(self.tsets['general']['endpoint'], self.tsets['general']['accesspoint'], self.tsets['general']['spacetoken']).get_output()
       self.assert_(self.st_result['status'] == 'PASS')
       self.sm1_result = space.StoRMGsm(self.tsets['general']['endpoint'], self.tsets['general']['accesspoint'], self.st_result['arrayOfSpaceTokens']).get_output()
