fadacd3f87f4a546a41c61d2af3d4a5b19047fe7
Elisabetta Ronchieri <elisabetta.ronchieri@cnaf.infn.it>
2011-06-30 14:22:40 +0000

    add bringonline cmd

---
+++ tstorm/utils/bringonline.py
@@ -0,0 +1,160 @@
+__author__ = 'Elisabetta Ronchieri'
+
+import commands
+import os
+from tstorm.utils import utils
+
+class LcgBringonline:
+  def __init__(self, endpoint, accesspoint, dfn):
+    self.endpoint = endpoint
+    self.accesspoint = accesspoint
+    self.dfn = dfn
+    self.cmd = {
+      'name': 'lcg-bringonline',
+      'protocol': 'srm'}
+    self.otpt = {
+      'status':''}
+
+  def get_command(self):
+    a= self.cmd['name'] + ' -b --verbose -T srmv2 '+ self.cmd['protocol'] + '://' + self.endpoint + ':8444/srm/managerv2?SFN=/' + self.accesspoint + self.dfn
+    print a
+    return a
+
+  def run_command(self):
+    a=()
+    if utils.cmd_exist(self.cmd['name']):
+      a=commands.getstatusoutput(self.get_command())
+    return a
+
+  def get_output(self):
+    a=self.run_command()
+    if len(a) > 0 and a[0] == 0:
+      self.otpt['status'] = 'PASS'
+    else:
+      self.otpt['status'] = 'FAILURE'
+    return self.otpt
+
+class SrmBringonline:
+  def __init__(self, endpoint, accesspoint, dfn):
+    self.endpoint = endpoint
+    self.accesspoint = accesspoint
+    self.dfn = dfn
+    self.cmd = {
+      'name': 'srm-bring-online',
+      'protocol': 'srm'}
+    self.otpt = {
+      'status':''}
+
+  def get_command(self):
+    a= self.cmd['name'] + ' -2 -debug '+ self.cmd['protocol'] + '://' + self.endpoint + ':8444/srm/managerv2?SFN=/' + self.accesspoint + self.dfn
+    print a
+    return a
+
+  def run_command(self):
+    a=()
+    if utils.cmd_exist(self.cmd['name']):
+      a=commands.getstatusoutput(self.get_command())
+    return a
+
+  def get_output(self):
+    a=self.run_command()
+    if len(a) > 0 and a[0] == 0:
+      self.otpt['status'] = 'PASS'
+    else:
+      self.otpt['status'] = 'FAILURE'
+    return self.otpt
+
+class StoRMBol:
+  def __init__(self, endpoint, accesspoint, dfn):
+    self.endpoint = endpoint
+    self.accesspoint = accesspoint
+    self.dfn = dfn
+    self.cmd = {
+      'name': 'clientSRM',
+      'rqst_protocol': 'httpg',
+      'protocol': 'srm'}
+    self.otpt = {
+      'status':'',
+      'statusCode':[],
+      'explanation':[]}
+
+  def get_command(self):
+    a = self.cmd['name'] + ' bol -e ' + self.cmd['rqst_protocol'] + '://' + self.endpoint + ':8444/' + ' -s ' + self.cmd['protocol'] + '://' + self.endpoint + ':8444/srm/managerv2?SFN=/' + self.accesspoint + self.dfn + ' -p'
+    print a
+    return a
+
+  def run_command(self):
+    a=()
+    if utils.cmd_exist(self.cmd['name']):
+      a=commands.getstatusoutput(self.get_command())
+    return a
+
+  def get_output(self):
+    a=self.run_command()
+    if len(a) > 0 and a[0] == 0:
+      if 'SRM_SUCCESS' in a[1]:
+        if 'SRM_NOT_SUPPORTED' in a[1]:
+          self.otpt['status'] = 'FAILURE'
+        else:
+          for x in self.otpt:
+            if x == 'status':
+              self.otpt['status'] = 'PASS'
+            else:
+              y = a[1].split('\n')
+              for z in y:
+                if x in z:
+                  self.otpt[x].append(z.split(x)[1].split('="')[1].split('"')[0])
+      else:
+        self.otpt['status'] = 'FAILURE'
+    else:
+      self.otpt['status'] = 'FAILURE'
+
+    return self.otpt
+
+class StoRMSbol:
+  def __init__(self, endpoint, accesspoint, dfn, turl):
+    self.endpoint = endpoint
+    self.accesspoint = accesspoint
+    self.dfn = dfn
+    self.turl = turl
+    self.cmd = {
+      'name': 'clientSRM',
+      'rqst_protocol': 'httpg',
+      'protocol': 'srm'}
+    self.otpt = {
+      'status':'',
+      'statusCode':[],
+      'explanation':[]}
+
+  def get_command(self):
+    a = self.cmd['name'] + ' sbol -e ' + self.cmd['rqst_protocol'] + '://' + self.endpoint + ':8444/' + ' -s ' + self.cmd['protocol'] + '://' + self.endpoint + ':8444/srm/managerv2?SFN=/' + self.accesspoint + self.dfn + ' -t ' + self.turl
+    print a
+    return a
+
+  def run_command(self):
+    a=()
+    if utils.cmd_exist(self.cmd['name']):
+      a=commands.getstatusoutput(self.get_command())
+    return a
+
+  def get_output(self):
+    a=self.run_command()
+    if len(a) > 0 and a[0] == 0:
+      if 'SRM_SUCCESS' in a[1]:
+        if 'SRM_NOT_SUPPORTED' in a[1]:
+          self.otpt['status'] = 'FAILURE'
+        else:
+          for x in self.otpt:
+            if x == 'status':
+              self.otpt['status'] = 'PASS'
+            else:
+              y = a[1].split('\n')
+              for z in y:
+                if x in z:
+                  self.otpt[x].append(z.split(x)[1].split('="')[1].split('"')[0])
+      else:
+        self.otpt['status'] = 'FAILURE'
+    else:
+      self.otpt['status'] = 'FAILURE'
+
+    return self.otpt
--- tstorm/utils/ls.py
+++ tstorm/utils/ls.py
@@ -14,7 +14,8 @@ class LcgLs:
       'protocol': 'srm'}
     self.otpt = {
       'status':'',
-      'Checksum': ''}
+      'Checksum': '',
+      'fileLocality': ''}
 
   def get_command(self):
     a= self.cmd['name'] + ' -l -b -D srmv2 '+ self.cmd['protocol'] + '://' + self.endpoint + ':8444/srm/managerv2?SFN=/' + self.accesspoint + self.dfn
@@ -34,9 +35,71 @@ class LcgLs:
       x=a[1].split('\n')
       k=self.otpt.keys()
       for y in x:
-        if 'Checksum' in y:
+        if 'ONLINE_AND_NEARLINE' in y:
+          self.otpt['fileLocality']='ONLINE_AND_NEARLINE'
+        elif 'ONLINE' in y:
+          self.otpt['fileLocality']='ONLINE'
+        elif 'NEARLINE' in y:
+          self.otpt['fileLocality']='NEARLINE'
+        elif 'Checksum' in y:
           self.otpt['Checksum'] = y.split(':')[1][1:]
     else:
       self.otpt['status'] = 'FAILURE'
+    return self.otpt
+
+class StoRMLs:
+  def __init__(self, endpoint, accesspoint, dfn):
+    self.endpoint = endpoint
+    self.accesspoint = accesspoint
+    self.dfn = dfn
+    self.cmd = {
+      'name': 'clientSRM',
+      'rqst_protocol': 'httpg',
+      'protocol': 'srm'}
+    self.otpt = {
+      'status':'',
+      'statusCode':[],
+      'explanation':[],
+      'fileLocality':'',
+      'checkSumType':'',
+      'checkSumValue':''}
+    self.map = {
+      '0': 'ONLINE',
+      '1': 'NEARLINE',
+      '2': 'ONLINE_AND_NEARLINE'}
+
+  def get_command(self):
+    a = self.cmd['name'] + ' ls -e ' + self.cmd['rqst_protocol'] + '://' + self.endpoint + ':8444/' + ' -s ' + self.cmd['protocol'] + '://' + self.endpoint + ':8444/srm/managerv2?SFN=/' + self.accesspoint + self.dfn
+    print a
+    return a
+
+  def run_command(self):
+    a=()
+    if utils.cmd_exist(self.cmd['name']):
+      a=commands.getstatusoutput(self.get_command())
+    return a
+
+  def get_output(self):
+    a=self.run_command()
+    if len(a) > 0 and a[0] == 0:
+      if 'SRM_SUCCESS' in a[1]:
+        for x in self.otpt:
+          if x == 'status':
+            self.otpt['status'] = 'PASS'
+          else:
+            y = a[1].split('\n')
+            for z in y:
+              if x in z:
+                if x == 'fileLocality':
+                  self.otpt[x] = self.map(c[z.split(x)[1].split('=')[1]])
+                elif x in ('checkSumType', 'checkSumValue'):
+                  self.otpt[x] = z.split(x)[1].split('="')[1].split('"')[0]
+                else:
+                  self.otpt[x].append(z.split(x)[1].split('="')[1].split('"')[0])
+      else:
+        self.otpt['status'] = 'FAILURE'
+    else:
+      self.otpt['status'] = 'FAILURE'
+
     return self.otpt
     
