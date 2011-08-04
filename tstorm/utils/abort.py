99034dbbce8d154c7feed2c2634a578f4bb229df
Elisabetta Ronchieri <elisabetta.ronchieri@cnaf.infn.it>
2011-08-04 15:15:58 +0000

    update ldap test

---
+++ tstorm/utils/abort.py
@@ -0,0 +1,50 @@
+__author__ = 'Elisabetta Ronchieri'
+
+import commands
+import os
+from tstorm.utils import utils
+
+class StoRMAbort:
+  def __init__(self, endpoint, accesspoint, dfn, turl):
+    self.endpoint = endpoint
+    self.accesspoint = accesspoint
+    self.dfn = dfn
+    self.turl = turl
+    self.cmd = {
+      'name': 'clientSRM',
+      'rqst_protocol': 'httpg',
+      'protocol': 'srm'}
+    self.otpt = {
+      'status':'',
+      'statusCode':[],
+      'explanation':[]}
+
+  def get_command(self):
+    a = self.cmd['name'] + ' ar -e ' + self.cmd['rqst_protocol'] + '://' + self.endpoint + ':8444/' + ' -s ' + self.cmd['protocol'] + '://' + self.endpoint + ':8444/srm/managerv2?SFN=/' + self.accesspoint + self.dfn + ' -t ' + self.turl
+    print a
+    return a
+
+  def run_command(self):
+    a=()
+    if utils.cmd_exist(self.cmd['name']):
+      a=commands.getstatusoutput(self.get_command())
+    return a
+
+  def get_output(self):
+    a=self.run_command()
+    if len(a) > 0 and a[0] == 0:
+      if 'SRM_SUCCESS' in a[1]:
+        for x in self.otpt:
+          if x == 'status':
+            self.otpt['status'] = 'PASS'
+          else:
+            y = a[1].split('\n')
+            for z in y:
+              if x in z:
+                self.otpt[x].append(z.split(x)[1].split('="')[1].split('"')[0])
+      else:
+        self.otpt['status'] = 'FAILURE'
+    else:
+      self.otpt['status'] = 'FAILURE'
+
+    return self.otpt
--- tstorm/utils/ldapsearch.py
+++ tstorm/utils/ldapsearch.py
@@ -5,17 +5,17 @@ import os
 from tstorm.utils import utils
 
 class LdapSearch:
-  def __init__(self, endpoint, basedn='mds-vo-name=resource,o=grid', filter="'objectClass=GlueService'", attributes):
+  def __init__(self, endpoint, attributes, basedn='mds-vo-name=resource,o=grid', filter="'objectClass=GlueService'"):
     self.endpoint = endpoint
     self.basedn = basedn
     self.filter = filter
     self.attributes = attributes
     self.cmd = {
       'name': 'ldapsearch',
-      'protocol': 'ldap'
-      'port': '2170'}
-      'basedn':'-b' + self.basedn,
-      'uri':'-H' + self.cmd['protocol'] + '://' + self.endpoint + ':' + self.cmd['port'],
+      'protocol': 'ldap',
+      'port': '2170',
+      'basedn':'-b' + ' ' + self.basedn,
+      'uri':'-H',
       'pres':'-LLL',
       'sa':'-x'}
     self.otpt = {
@@ -24,19 +24,20 @@ class LdapSearch:
       'GlueServiceType':''}
 
   def get_command(self):
-    opt = ' ' + self.cmd['sa'] + ' ' + self.cmd['pres'] + ' ' + self.cmd['uri'] + ' ' +  self.cmd['basedn'] + ' '
-    a= self.cmd['name'] + opt + self.filter + ' ' self.attributes
+    uri = self.cmd['uri'] + ' ' + self.cmd['protocol'] + '://' + self.endpoint + ':' + self.cmd['port']
+    opt = ' ' + self.cmd['sa'] + ' ' + self.cmd['pres'] + ' ' + uri + ' ' +  self.cmd['basedn'] + ' '
+    a= self.cmd['name'] + opt + self.filter + ' ' + self.attributes
       
     print a
     return a
 
-  def run_command(self, in_write=True):
+  def run_command(self):
     a=()
     if utils.cmd_exist(self.cmd["name"]):
       a=commands.getstatusoutput(self.get_command())
     return a
 
-  def get_output(self, in_write=True):
+  def get_output(self):
     a=self.run_command()
     if len(a) > 0 and a[0] == 0:
       for x in self.otpt:
