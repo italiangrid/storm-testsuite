5c9c392c927fb5a532ea42e130e1e39f6abc3a1e
Elisabetta Ronchieri <elisabetta.ronchieri@cnaf.infn.it>
2011-08-13 17:24:42 +0000

    added other regression tests

--- tstorm/test/regression_conf.py
+++ tstorm/test/regression_conf.py
@@ -2,7 +2,7 @@ __author__ = 'Elisabetta Ronchieri'
 
 import os 
 import unittest
-from tstorm.utils import cat
+from tstorm.utils import readfile
 
 class RegressionConfigurationTest(unittest.TestCase):
     def __init__(self):
@@ -11,7 +11,7 @@ class RegressionConfigurationTest(unittest.TestCase):
     def test_backend_logrotate_file(self):
       '''SYSTEM TEST - REGRESSION TESTS'''
       '''3.4.8 - RfC https://storm.cnaf.infn.it:8443/redmine/issues/134'''
-      self.cat_result = cat.Cat('/etc/logrotate.d/storm-backend-server').get_output()
+      self.cat_result = readfile.Cat('/etc/logrotate.d/storm-backend-server').get_output()
       self.assert_(self.cat_result['status'] == 'PASS')
       self.assert_('/opt/storm/backend/var/log/storm-backend.stdout' not in self.cat_result['otpt'])
       self.assert_('/opt/storm/backend/var/log/storm-backend.stderr' not in self.cat_result['otpt'])
@@ -23,7 +23,7 @@ class RegressionConfigurationTest(unittest.TestCase):
     def test_backend_cron_file(self):
       '''SYSTEM TEST - REGRESSION TESTS'''
       '''3.4.9 - RfC https://storm.cnaf.infn.it:8443/redmine/issues/135'''
-      self.cat_result = cat.Cat('/etc/cron.d/storm-backend-server.cron').get_output()
+      self.cat_result = readfile.Cat('/etc/cron.d/storm-backend-server.cron').get_output()
       self.assert_(self.cat_result['status'] == 'PASS')
       self.assert_('/opt/storm/backend/etc/logrotate.d/logrotate.status' not in self.cat_result['otpt'])
       self.assert_('/opt/storm/backend/etc/logrotate.d/storm-backend.logrotate' not in self.cat_result['otpt'])
