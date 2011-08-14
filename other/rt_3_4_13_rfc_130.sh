cab0d90725287159d94e462f2f1f1c7760e5d8d5
Elisabetta Ronchieri <elisabetta.ronchieri@cnaf.infn.it>
2011-08-14 17:16:53 +0000

    updated tstorm-etics tests

--- other/rt_3_4_13_rfc_130.sh
+++ other/rt_3_4_13_rfc_130.sh
@@ -1,5 +1,13 @@
 #/bin/sh 
 
+cv=$1
+pv=$2
+
+# change . and - with _
+ncv=`echo $cv | sed -e 's/\./_/g' | sed -e 's/\-/_/g'`
+# remove age from cv
+cr=`echo $cv | awk -F"-" '{ print $1 }'`
+
 echo 'RT 3.3.13 RFC https://storm.cnaf.infn.it:8443/redmine/issues/130'
 
 echo 'mkdir -p storm-gridhttps-plugin'
@@ -11,17 +19,17 @@ cd storm-gridhttps-plugin/ &> /tmp/rt_3.3.13_rfc_130.txt
 echo 'etics-workspace-setup'
 etics-workspace-setup &> /tmp/rt_3.3.13_rfc_130.txt
 
-echo 'etics-get-project emi'
-etics-get-project emi &> /tmp/rt_3.3.13_rfc_130.txt
+echo 'etics-get-project --noask emi'
+etics-get-project --noask emi &> /tmp/rt_3.3.13_rfc_130.txt
 
-echo 'etics-checkout --platform "sl5_x86_64_gcc412EPEL" --verbose --project "emi" --project-config emi_R_1_rc --config emi-storm-gridhttps-plugin_R_1_0_2_1 emi.storm.gridhttps-plugin'
-etics-checkout --platform "sl5_x86_64_gcc412EPEL" --verbose --project "emi" --project-config emi_R_1_rc --config emi-storm-gridhttps-plugin_R_1_0_2_1 emi.storm.gridhttps-plugin &> /tmp/rt_3.3.13_rfc_130.txt
+echo etics-checkout --platform "sl5_x86_64_gcc412EPEL" --verbose --noask --project "emi" --project-config emi_R_$pv\_rc --config emi-storm-gridhttps-plugin_$ncv emi.storm.gridhttps-plugin
+etics-checkout --platform "sl5_x86_64_gcc412EPEL" --verbose --noask --project "emi" --project-config emi_R_$pv\_rc --config emi-storm-gridhttps-plugin_R_$ncv emi.storm.gridhttps-plugin &> /tmp/rt_3.3.13_rfc_130.txt
 
-echo 'etics-build --platform "sl5_x86_64_gcc412EPEL" --verbose --config emi-storm-gridhttps-plugin_R_1_0_2_1 emi.storm.gridhttps-plugin'
-etics-build --platform "sl5_x86_64_gcc412EPEL" --verbose --config emi-storm-gridhttps-plugin_R_1_0_2_1 emi.storm.gridhttps-plugin &> /tmp/rt_3.3.13_rfc_130.txt
+echo etics-build --platform "sl5_x86_64_gcc412EPEL" --verbose --config emi-storm-gridhttps-plugin_R_$ncv emi.storm.gridhttps-plugin
+etics-build --platform "sl5_x86_64_gcc412EPEL" --verbose --config emi-storm-gridhttps-plugin_R_$ncv emi.storm.gridhttps-plugin &> /tmp/rt_3.3.13_rfc_130.txt
 
-echo 'ls dist/emi/emi.storm.gridhttps-plugin/1.0.2/sl5_x86_64_gcc412EPEL/storm-gridhttps-plugin-1.0.2-1.sl5.noarch.rpm'
-a=`ls dist/emi/emi.storm.gridhttps-plugin/1.0.2/sl5_x86_64_gcc412EPEL/storm-gridhttps-plugin-1.0.2-1.sl5.noarch.rpm`
+echo ls dist/emi/emi.storm.gridhttps-plugin/$cr/sl5_x86_64_gcc412EPEL/storm-gridhttps-plugin-$cv.sl5.noarch.rpm
+a=`ls dist/emi/emi.storm.gridhttps-plugin/$cr/sl5_x86_64_gcc412EPEL/storm-gridhttps-plugin-$cv.sl5.noarch.rpm`
 
 echo $a | grep -q "noarch" && echo "PASS" || echo "FAIL"
 rm /tmp/rt_3.3.13_rfc_130.txt
--- other/rt_3_4_14_rfc_131.sh
+++ other/rt_3_4_14_rfc_131.sh
@@ -1,5 +1,13 @@
 #/bin/sh 
 
+cv=$1
+pv=$2
+
+# change . and - with _
+ncv=`echo $cv | sed -e 's/\./_/g' | sed -e 's/\-/_/g'`
+# remove age from cv
+cr=`echo $cv | awk -F"-" '{ print $1 }'`
+
 echo 'RT 3.3.14 RFC https://storm.cnaf.infn.it:8443/redmine/issues/131'
 
 echo 'mkdir -p storm-gridhttps-server'
@@ -11,17 +19,17 @@ cd storm-gridhttps-server/ &> /tmp/rt_3.3.14_rfc_131.txt
 echo 'etics-workspace-setup'
 etics-workspace-setup &> /tmp/rt_3.3.14_rfc_131.txt
 
-echo 'etics-get-project emi'
-etics-get-project emi &> /tmp/rt_3.3.14_rfc_131.txt
+echo 'etics-get-project --noask emi'
+etics-get-project --noask emi &> /tmp/rt_3.3.14_rfc_131.txt
 
-echo 'etics-checkout --platform "sl5_x86_64_gcc412EPEL" --verbose --project "emi" --project-config emi_R_1_rc --config emi-storm-gridhttps-server_R_1_0_4_1 emi.storm.gridhttps-server'
-etics-checkout --platform "sl5_x86_64_gcc412EPEL" --verbose --project "emi" --project-config emi_R_1_rc --config emi-storm-gridhttps-server_R_1_0_4_1 emi.storm.gridhttps-server &> /tmp/rt_3.3.14_rfc_131.txt
+echo etics-checkout --platform "sl5_x86_64_gcc412EPEL" --verbose --noask --project "emi" --project-config emi_R_$pv\_rc --config emi-storm-gridhttps-server_R_$ncv emi.storm.gridhttps-server
+etics-checkout --platform "sl5_x86_64_gcc412EPEL" --verbose --noask --project "emi" --project-config emi_R_$pv\_rc --config emi-storm-gridhttps-server_R_$ncv emi.storm.gridhttps-server &> /tmp/rt_3.3.14_rfc_131.txt
 
-echo 'etics-build --platform "sl5_x86_64_gcc412EPEL" --verbose --config emi-storm-gridhttps-server_R_1_0_4_1 emi.storm.gridhttps-server'
-etics-build --platform "sl5_x86_64_gcc412EPEL" --verbose --config emi-storm-gridhttps-server_R_1_0_4_1 emi.storm.gridhttps-server &> /tmp/rt_3.3.14_rfc_131.txt
+echo etics-build --platform "sl5_x86_64_gcc412EPEL" --verbose --config emi-storm-gridhttps-server_R_$ncv emi.storm.gridhttps-server
+etics-build --platform "sl5_x86_64_gcc412EPEL" --verbose --config emi-storm-gridhttps-server_R_$ncv emi.storm.gridhttps-server &> /tmp/rt_3.3.14_rfc_131.txt
 
-echo 'ls dist/emi/emi.storm.gridhttps-server/1.0.4/sl5_x86_64_gcc412EPEL/storm-gridhttps-server-1.0.4-1.sl5.noarch.rpm'
-a=`ls dist/emi/emi.storm.gridhttps-server/1.0.4/sl5_x86_64_gcc412EPEL/storm-gridhttps-server-1.0.4-1.sl5.noarch.rpm`
+echo ls dist/emi/emi.storm.gridhttps-server/$cr/sl5_x86_64_gcc412EPEL/storm-gridhttps-server-$cv.sl5.noarch.rpm
+a=`ls dist/emi/emi.storm.gridhttps-server/$cr/sl5_x86_64_gcc412EPEL/storm-gridhttps-server-$cv.sl5.noarch.rpm`
 
 echo $a | grep -q "noarch" && echo "PASS" || echo "FAIL"
-#rm /tmp/rt_3.3.14_rfc_131.txt
+rm /tmp/rt_3.3.14_rfc_131.txt
