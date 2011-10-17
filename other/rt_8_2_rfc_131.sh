2c9330295309e1da74d51c7d351c5f903311882c
Elisabetta Ronchieri <elisabetta.ronchieri@cnaf.infn.it>
2011-10-17 14:27:18 +0000

    updated checks

--- other/rt_8_2_rfc_131.sh
+++ other/rt_8_2_rfc_131.sh
@@ -8,28 +8,33 @@ ncv=`echo $cv | sed -e 's/\./_/g' | sed -e 's/\-/_/g'`
 # remove age from cv
 cr=`echo $cv | awk -F"-" '{ print $1 }'`
 
-echo 'RT 3.4.14 RFC https://storm.cnaf.infn.it:8443/redmine/issues/131'
+echo 'Name: Non Compliant StoRM GridHttps Server RPM name'
+#echo 'RT 3.4.14 RFC https://storm.cnaf.infn.it:8443/redmine/issues/131'
+
+echo 'Description: The RPM binary name of the storm-gridhttps-server component does not contain architecture after the OS name.'
+
+echo 'Output:'
 
 echo 'mkdir -p storm-gridhttps-server'
-mkdir -p storm-gridhttps-server &> /tmp/rt_3.3.14_rfc_131.txt 
+mkdir -p storm-gridhttps-server &> /tmp/rt_8_2_rfc_131.txt 
 
 echo 'cd storm-gridhttps-server/'
-cd storm-gridhttps-server/ &> /tmp/rt_3.3.14_rfc_131.txt
+cd storm-gridhttps-server/ &> /tmp/rt_8_2_rfc_131.txt
 
 echo 'etics-workspace-setup'
-etics-workspace-setup &> /tmp/rt_3.3.14_rfc_131.txt
+etics-workspace-setup &> /tmp/rt_8_2_rfc_131.txt
 
 echo 'etics-get-project --noask emi'
 etics-get-project --noask emi &> /tmp/rt_3.3.14_rfc_131.txt
 
 echo etics-checkout --platform "sl5_x86_64_gcc412EPEL" --verbose --noask --project "emi" --project-config emi_R_$pv\_rc --config emi-storm-gridhttps-server_R_$ncv emi.storm.gridhttps-server
-etics-checkout --platform "sl5_x86_64_gcc412EPEL" --verbose --noask --project "emi" --project-config emi_R_$pv\_rc --config emi-storm-gridhttps-server_R_$ncv emi.storm.gridhttps-server &> /tmp/rt_3.3.14_rfc_131.txt
+etics-checkout --platform "sl5_x86_64_gcc412EPEL" --verbose --noask --project "emi" --project-config emi_R_$pv\_rc --config emi-storm-gridhttps-server_R_$ncv emi.storm.gridhttps-server &> /tmp/rt_8_2_rfc_131.txt
 
 echo etics-build --platform "sl5_x86_64_gcc412EPEL" --verbose --config emi-storm-gridhttps-server_R_$ncv emi.storm.gridhttps-server
-etics-build --platform "sl5_x86_64_gcc412EPEL" --verbose --config emi-storm-gridhttps-server_R_$ncv emi.storm.gridhttps-server &> /tmp/rt_3.3.14_rfc_131.txt
+etics-build --platform "sl5_x86_64_gcc412EPEL" --verbose --config emi-storm-gridhttps-server_R_$ncv emi.storm.gridhttps-server &> /tmp/rt_8_2_rfc_131.txt
 
 echo ls dist/emi/emi.storm.gridhttps-server/$cr/sl5_x86_64_gcc412EPEL/storm-gridhttps-server-$cv.sl5.noarch.rpm
 a=`ls dist/emi/emi.storm.gridhttps-server/$cr/sl5_x86_64_gcc412EPEL/storm-gridhttps-server-$cv.sl5.noarch.rpm`
 
-echo $a | grep -q "noarch" && echo "PASS" || echo "FAIL"
-rm /tmp/rt_3.3.14_rfc_131.txt
+echo $a | grep -q "noarch" && echo "PASSED" || echo "FAILD"
+rm /tmp/rt_8_2_rfc_131.txt
---
+++ other/rt_8_3_rfc_164.sh
@@ -0,0 +1,40 @@
+#/bin/sh 
+
+cv=$1
+pv=$2
+
+# change . and - with _
+ncv=`echo $cv | sed -e 's/\./_/g' | sed -e 's/\-/_/g'`
+# remove age from cv
+cr=`echo $cv | awk -F"-" '{ print $1 }'`
+
+echo 'Name: No StoRM Backend Server Src RPM'
+#echo 'RT 8.3 RFC https://storm.cnaf.infn.it:8443/redmine/issues/164'
+
+echo 'Description: The source RPM of the storm-backend-server component is not created at build time.'
+
+echo 'Output:'
+
+echo 'mkdir -p storm-backend-server'
+mkdir -p storm-backend-server &> /tmp/rt_8_3_rfc_164.txt 
+
+echo 'cd storm-backend-server/'
+cd storm-backend-server/ &> /tmp/rt_8_3_rfc_164.txt
+
+echo 'etics-workspace-setup'
+etics-workspace-setup &> /tmp/rt_8_3_rfc_164.txt
+
+echo 'etics-get-project --noask emi'
+etics-get-project --noask emi &> /tmp/rt_8_3_rfc_164.txt
+
+echo etics-checkout --platform "sl5_x86_64_gcc412EPEL" --verbose --noask --project "emi" --project-config emi_R_$pv\_rc --config emi-storm-backend-server_B_1_7 emi.storm.backend-server
+etics-checkout --platform "sl5_x86_64_gcc412EPEL" --verbose --noask --project "emi" --project-config emi_R_$pv\_rc --config emi-storm-backend-server_B_1_7 emi.storm.backend-server &> /tmp/rt_8_3_rfc_164.txt
+
+echo etics-build --createsource --platform "sl5_x86_64_gcc412EPEL" --verbose --config emi-storm-backend-server_B_1_7 emi.storm.backend-server
+etics-build --platform "sl5_x86_64_gcc412EPEL" --verbose --config emi-storm-backend-server_B_1_7 emi.storm.backend-server &> /tmp/rt_8_3_rfc_164.txt
+
+echo ls dist/emi/emi.storm.backend-server/$cr/src/storm-backend-server-$cv.sl5.src.rpm
+a=`ls dist/emi/emi.storm.backend-server/$cr/src/storm-backend-server-$cv.sl5.src.rpm`
+
+echo $a | grep -q "src" && echo "PASSED" || echo "FAILD"
+rm /tmp/rt_8_3_rfc_164.txt
