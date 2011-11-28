e469312cfd8e8240df9bf2a87ef587c0303861cf
Elisabetta Ronchieri <elisabetta.ronchieri@cnaf.infn.it>
2011-11-28 09:12:47 +0000

    removed print

--- tstorm/__init__.py
+++ tstorm/__init__.py
@@ -4,10 +4,8 @@ VERSION = (1, 0, 8, 1)
 
 def get_version():
     version = '%s.%s.%s-%s' % (VERSION[0], VERSION[1], VERSION[2], VERSION[3])
-    print version 
     from tstorm.utils.version import get_svn_revision
     svn_rev = get_svn_revision()
     if svn_rev != u'SVN-unknown':
         version = "%s %s" % (version, svn_rev)
-    print version
     return version
