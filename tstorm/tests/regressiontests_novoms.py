a6873aefdfd8cee076accb2539c5bfd633d04b1c
Elisabetta Ronchieri <elisabetta.ronchieri@cnaf.infn.it>
2012-02-01 16:45:57 +0000

    readded a file that was in trunk but not in the branch

---
+++ tstorm/tests/regressiontests_novoms.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+__author__ = 'Elisabetta Ronchieri'
+
+import unittest
+from tstorm.test import regression as re
+
+def get_space_metadata_failure_ts(conf, ifn, dfn, bifn, uid, lfn):
+    s = unittest.TestSuite()
+    s.addTest(re.RegressionTest('test_get_space_metadata_failure',conf, ifn, dfn, bifn, uid, lfn))
+
+    return s
