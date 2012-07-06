b53a7488b458d05aa9383901f2bb9d1e6bdf9b83
Elisabetta Ronchieri <elisabetta.ronchieri@cnaf.infn.it>
2012-07-06 17:42:16 +0000

    modified list to print rfc

--- tstorm/utils/tests_info.py
+++ tstorm/utils/tests_info.py
@@ -11,13 +11,13 @@ class TestsInfo:
         print 'ID        NAME'
         for key, value in self.mti_info.items():
             if 'ts' in key and value[1] != 'DT':
-                print '%s    %s' % (value[0], value[5])
+                print '%s    %s' % (value[0], value[2])
 
     def __print_all_sanity_ids(self):
         print 'ID        NAME'
         for key, value in self.mti_info.items():
             if 'ts' in key and value[1] == 'DT':
-                print '%s    %s' % (value[0], value[5])
+                print '%s    %s' % (value[0], value[2])
 
     def __print_with_filters(self):
         filter_info = []
@@ -30,7 +30,7 @@ class TestsInfo:
                             str(self.info['r']).lower() == str(value[2]).lower() and \
                             str(self.info['i']).lower() == str(value[3]).lower():
                             filter_info.append(value[0])
-                            print '%s    %s' % (value[0], value[5])
+                            print '%s    %s' % (value[0], value[2])
         elif 't' in self.info.keys() and 'r' in self.info.keys():
             print 'ID        NAME'
             for key, value in self.mti_info.items():
@@ -39,7 +39,7 @@ class TestsInfo:
                         if x == value[1] and \
                             str(self.info['r']).lower() == str(value[2]).lower():
                             filter_info.append(value[0])
-                            print '%s    %s' % (value[0], value[5])
+                            print '%s    %s' % (value[0], value[2])
         elif 't' in self.info.keys() and 'i' in self.info.keys():
             print 'ID        NAME'
             for key, value in self.mti_info.items():
@@ -48,7 +48,7 @@ class TestsInfo:
                         if x == value[1] and \
                             str(self.info['i']).lower() == str(value[3]).lower():
                             filter_info.append(value[0])
-                            print '%s    %s' % (value[0], value[5])
+                            print '%s    %s' % (value[0], value[2])
         elif 'r' in self.info.keys() and 'i' in self.info.keys():
             print 'ID        NAME'
             for key, value in self.mti_info.items():
@@ -56,7 +56,7 @@ class TestsInfo:
                     if str(self.info['r']).lower() == str(self.value[2]).lower() and \
                         str(self.info['i']).lower() == str(value[3]).lower():
                         filter_info.append(value[0])
-                        print '%s    %s' % (value[0], value[5])
+                        print '%s    %s' % (value[0], value[2])
         elif 't' in self.info.keys():
             print 'ID        NAME'
             for key, value in self.mti_info.items():
@@ -64,21 +64,21 @@ class TestsInfo:
                     for x in self.info['t']:
                         if x == value[1]:
                             filter_info.append(value[0])
-                            print '%s    %s' % (value[0], value[5])
+                            print '%s    %s' % (value[0], value[2])
         elif 'r' in self.info.keys():
             print 'ID        NAME'
             for key, value in self.mti_info.items():
                 if 'ts' in key:
                     if str(self.info['r']).lower() == str(value[3]).lower():
                         filter_info.append(value[0])
-                        print '%s    %s' % (value[0], value[5])
+                        print '%s    %s' % (value[0], value[2])
         elif 'i' in self.info.keys():
             print 'ID        NAME'
             for key, value in self.mti_info.items():
                 if 'ts' in key:
                     if str(self.info['i']).lower() == str(value[3]).lower():
                         filter_info.append(value[0])
-                        print '%s    %s' % (value[0], value[5])
+                        print '%s    %s' % (value[0], value[2])
         if 'o' in self.info.keys():
             df = open(self.info['o'], 'w')
             for id in filter_info:
