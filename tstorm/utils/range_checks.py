06e475ef37b79a93dbc733d34236c0be1ce6b676
Elisabetta Ronchieri <elisabetta.ronchieri@cnaf.infn.it>
2012-07-11 16:59:31 +0000

    fixed range_checks

--- tstorm/utils/range_checks.py
+++ tstorm/utils/range_checks.py
@@ -16,7 +16,7 @@ class RangeChecks:
         return False
 
     def __check_release(self, val):
-        if len(val) == 4:
+        if len(val.split('.')) == 4:
             return True
         return False
 
@@ -38,47 +38,74 @@ class RangeChecks:
         return True
 
     def __check_type(self, val):
-        if self.__check_int_type(val):
-            return True
         if self.__check_str_type(val):
+            return True
+        if self.__check_int_type(val):
             return True
         return False
 
-    def __check_extreme_inf(self):
-        if self.__check_int_type(self.range[0]) and self.__check_int_type(self.range[1]):
-            if self.range[0] > self.range[1]:
-                return False
-        return True
-
     def __check_run_test(self):
         if self.inf == '(' and self.sup == ')':
-            if self.range[0] > self.revision and self.revision < self.range[1]:
+            if self.__check_str_type(self.range[0]) and self.__check_str_type(self.range[1]):
                 return True
-        if self.inf == '(' and self.sup == ']':
-            if self.range[0] > self.revision and self.revision <= self.range[1]:
+            elif self.__check_str_type(self.range[0]) and self.__check_int_type(self.range[1]):
+                if self.storm_revision < self.range[1]:
+                    return True
+            elif self.__check_int_type(self.range[0]) and self.__check_str_type(self.range[1]):
+                if self.storm_revision > self.range[0]:
+                    return True
+            elif self.__check_int_type(self.range[0]) and self.__check_int_type(self.range[1]):
+                if self.storm_revision > self.range[0] and self.storm_revision < self.range[1]:
+                    return True
+        elif self.inf == '(' and self.sup == ']':
+            if self.__check_str_type(self.range[0]) and self.__check_str_type(self.range[1]):
                 return True
-        if self.inf == '[' and self.sup == ')':
-            if self.range[0] >= self.revision and self.revision < self.range[1]:
+            elif self.__check_str_type(self.range[0]) and self.__check_int_type(self.range[1]):
+                if self.storm_revision <= self.range[1]:
+                    return True
+            elif self.__check_int_type(self.range[0]) and self.__check_str_type(self.range[1]):
+                if self.storm_revision > self.range[0]:
+                    return True
+            elif self.__check_int_type(self.range[0]) and self.__check_int_type(self.range[1]):
+                if self.storm_revision > self.range[0] and self.storm_revision <= self.range[1]:
+                    return True
+        elif self.inf == '[' and self.sup == ')':
+            if self.__check_str_type(self.range[0]) and self.__check_str_type(self.range[1]):
                 return True
-        if self.inf == '[' and self.sup == ']':
-            if self.range[0] >= self.revision and self.revision <= self.range[1]:
+            elif self.__check_str_type(self.range[0]) and self.__check_int_type(self.range[1]):
+                if self.storm_revision < self.range[1]:
+                    return True
+            elif self.__check_int_type(self.range[0]) and self.__check_str_type(self.range[1]):
+                if self.storm_revision >= self.range[0]:
+                    return True
+            elif self.__check_int_type(self.range[0]) and self.__check_int_type(self.range[1]):
+                if self.storm_revision >= self.range[0] and self.storm_revision < self.range[1]:
+                    return True
+        elif self.inf == '[' and self.sup == ']':
+            if self.__check_str_type(self.range[0]) and self.__check_str_type(self.range[1]):
                 return True
+            elif self.__check_str_type(self.range[0]) and self.__check_int_type(self.range[1]):
+                if self.storm_revision <= self.range[1]:
+                    return True
+            elif self.__check_int_type(self.range[0]) and self.__check_str_type(self.range[1]):
+                if self.storm_revision >= self.range[0]:
+                    return True
+            elif self.__check_int_type(self.range[0]) and self.__check_int_type(self.range[1]):
+                if self.storm_revision >= self.range[0] and self.storm_revision <= self.range[1]:
+                    return True
+
         return False
 
     def is_valid(self):
         if not self.__check_outer():
             return False
-        if not self.__check_release(self.release):
+        if not self.__check_release(self.storm_release):
             return False
         if not self.__check_size():
-            return False
-        if not self.__check_release(self.range[0]):
             return False
         for x in self.range:
             if not self.__check_type(x):
                 return False
-        if not self.__check_extreme_inf():
-            return False 
         if not self.__check_run_test():
             return False
         return True
