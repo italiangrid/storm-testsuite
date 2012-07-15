499d91de3c95e4953f246b0de556fb5605d81094
Elisabetta Ronchieri <elisabetta.ronchieri@cnaf.infn.it>
2012-07-15 09:19:41 +0000

    improved binary

---
+++ tstorm/utils/sequence.py
@@ -0,0 +1,16 @@
+import sys
+import os
+
+class SequenceError:
+    def __init__(self, msg):
+        self.args = msg
+        self.errmsg = msg
+
+class Sequence:
+    def __init__(self, value):
+        if ' ' in value:
+            raise SequenceError('The sequence contains empty space')
+        self.sequence = [x.strip() for x in value.split(',')])
+
+    def get_sequence(self):
+        return self.sequence
