414313a42b49a75b6f30b06bbf6e6fefd74630f3
Elisabetta Ronchieri <elisabetta.ronchieri@cnaf.infn.it>
2012-07-17 15:58:51 +0000

    re-included some atomic tests in the json file

--- tstorm/tests/atomic/atomicstests.py
+++ tstorm/tests/atomic/atomicstests.py
@@ -4,6 +4,11 @@ import unittest
 from tstorm.tests.atomic import atomics
 from tstorm.tests import utilities
 
+def ts_dcache_ping(conf, ifn, dfn, bifn, uid, lfn):
+    s = unittest.TestSuite()
+    s.addTest(atomics.AtomicsTest('test_dcache_ping',conf, ifn, dfn, bifn, lfn))
+    return s
+
 def ts_storm_ping(conf, ifn, dfn, bifn, uid, lfn):
     s = unittest.TestSuite()
     s.addTest(atomics.AtomicsTest('test_storm_ping',conf, ifn, dfn, bifn, lfn))
@@ -29,6 +34,11 @@ def ts_lcg_ls_unexist_file(conf, ifn, dfn, bifn, uid, lfn):
     s.addTest(atomics.AtomicsTest('test_lcg_ls_unexist_file',conf, ifn, dfn, bifn, lfn))
     return s
 
+def ts_lcg_ls_unexist_dir(conf, ifn, dfn, bifn, uid, lfn):
+    s = unittest.TestSuite()
+    s.addTest(atomics.AtomicsTest('test_lcg_ls_unexist_dir',conf, ifn, dfn, bifn, lfn))
+    return s
+
 def ts_dcache_mkdir(conf, ifn, dfn, bifn, uid, lfn):
     s = unittest.TestSuite()
     s.addTest(atomics.AtomicsTest('test_dcache_mkdir',conf, ifn, dfn, bifn, lfn))
@@ -36,6 +46,13 @@ def ts_dcache_mkdir(conf, ifn, dfn, bifn, uid, lfn):
     return s
 
 def ts_dcache_mkdir_exist(conf, ifn, dfn, bifn, uid, lfn):
+    s = unittest.TestSuite()
+    s.addTest(atomics.AtomicsTest('test_dcache_mkdir',conf, ifn, dfn, bifn, lfn))
+    s.addTest(atomics.AtomicsTest('test_dcache_mkdir_exist_dir',conf, ifn, dfn, bifn, lfn))
+    s.addTest(atomics.AtomicsTest('test_dcache_rm_dir',conf, ifn, dfn, bifn, lfn))
+    return s
+
+def ts_dcache_rm_dir(conf, ifn, dfn, bifn, uid, lfn):
     s = unittest.TestSuite()
     s.addTest(atomics.AtomicsTest('test_dcache_mkdir',conf, ifn, dfn, bifn, lfn))
     s.addTest(atomics.AtomicsTest('test_dcache_mkdir_exist_dir',conf, ifn, dfn, bifn, lfn))
@@ -85,16 +102,16 @@ def ts_dcache_rm_file(conf, ifn, dfn, bifn, uid, lfn):
 
 def ts_dcache_rm_unexist_file(conf, ifn, dfn, bifn, uid, lfn):
     s = unittest.TestSuite()
-    s.addTest(utilities.UtilitiesTest('test_dd',conf, ifn, bifn, lfn))
+    #s.addTest(utilities.UtilitiesTest('test_dd',conf, ifn, bifn, lfn))
     s.addTest(atomics.AtomicsTest('test_dcache_rm_unexist_file',conf, ifn, dfn, bifn, lfn)) 
-    s.addTest(utilities.UtilitiesTest('test_rm_lf',conf, ifn, bifn, lfn))
+    #s.addTest(utilities.UtilitiesTest('test_rm_lf',conf, ifn, bifn, lfn))
     return s
 
 def ts_dcache_rm_unexist_dir(conf, ifn, dfn, bifn, uid, lfn):
     s = unittest.TestSuite()
-    s.addTest(utilities.UtilitiesTest('test_dd',conf, ifn, bifn, lfn))
+    #s.addTest(utilities.UtilitiesTest('test_dd',conf, ifn, bifn, lfn))
     s.addTest(atomics.AtomicsTest('test_dcache_rm_unexist_dir',conf, ifn, dfn, bifn, lfn))
-    s.addTest(utilities.UtilitiesTest('test_rm_lf',conf, ifn, bifn, lfn))
+    #s.addTest(utilities.UtilitiesTest('test_rm_lf',conf, ifn, bifn, lfn))
     return s
 
 def ts_lcg_cp_in(conf, ifn, dfn, bifn, uid, lfn):
