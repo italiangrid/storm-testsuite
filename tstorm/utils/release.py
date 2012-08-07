2f349fe2693ca124709415030578f421f8e554fd
Elisabetta Ronchieri <elisabetta.ronchieri@cnaf.infn.it>
2012-08-07 14:55:02 +0000

    Removed unused import and unnecessary print

--- tstorm/utils/release.py
+++ tstorm/utils/release.py
@@ -1,5 +1,3 @@
-import sys
-import os
 import exceptions
 
 class ReleaseError(exceptions.Exception):
@@ -34,72 +32,49 @@ class Release:
     def is_greater(self,value):
         if self.infinity:
             if value.is_infinity():
-                print '1'
                 return False
             else:
-                print '2'
                 return True
         elif value.is_infinity():
-            print '3'
             return False
         elif self.release > value.get_release():
-            print '4'
             return True
         else:
-            print '5'
             return False
 
     def is_greater_and_equal(self,value):
         if self.infinity:
             if value.is_infinity():
-                print '6'
                 return True
             else:
-                print '7'
                 return False
         elif value.is_infinity():
-            print '8'
             return False
         elif self.release >= value.get_release():
-            print '9'
             return True
         else:
-            print '10'
             return False
 
     #returns tue if this objctr is lower than the provided parameter that is a Release object  
     def is_lower(self,value):
         if self.infinity:
-            #if value.is_infinity():
-            #    return False
-            #else:
-            #    return False
-            print '11'
             return False
         elif value.is_infinity():
-            print '12'
             return True
         elif self.release < value.get_release():
-            print '13'
             return True
         else:
-            print '14'
             return False
 
     def is_lower_and_equal(self,value):
         if self.infinity:
             if value.is_infinity():
-                print '15'
                 return True
             else:
-                print '16'
                 return False
         elif value.is_infinity():
-            print '17'
             return True
         elif self.release <= value.get_release():
-            print '18'
             return True
         else:
-            print '19'
             return False
