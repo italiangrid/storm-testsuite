7a75df86c3231c47d770ec8d1214aaa0c03973c8
Elisabetta Ronchieri <elisabetta.ronchieri@cnaf.infn.it>
2012-08-07 15:48:27 +0000

    Improved a couple of tests

--- tstorm/utils/unittest/limit_test.py
+++ tstorm/utils/unittest/limit_test.py
@@ -9,10 +9,8 @@ from tstorm.utils import limit
 
 class LimitTest(unittest.TestCase):
 
-
     def setUp(self):
         pass
-
 
     def tearDown(self):
         pass
@@ -43,48 +41,48 @@ class LimitTest(unittest.TestCase):
         pass
     
     def test_if_extreme_included_is_inf(self):
-        myLimit = limit.Limit('[')
-        self.failIf(not myLimit.is_inf())
+        my_limit = limit.Limit('[')
+        self.failIf(not my_limit.is_inf())
         pass
     
     def test_if_extreme_included_is_not_sup(self):
-        myLimit = limit.Limit('[')
-        self.failIf(myLimit.is_sup())
+        my_limit = limit.Limit('[')
+        self.failIf(my_limit.is_sup())
         pass
     
     def test_if_is_inf(self):
-        myLimit = limit.Limit('(')
-        self.failIf(not myLimit.is_inf())
+        my_limit = limit.Limit('(')
+        self.failIf(not my_limit.is_inf())
         pass
     
     def test_if_is_not_sup(self):
-        myLimit = limit.Limit('(')
-        self.failIf(myLimit.is_sup())
+        my_limit = limit.Limit('(')
+        self.failIf(my_limit.is_sup())
         pass
     
     def test_if_extreme_included_is_sup(self):
-        myLimit = limit.Limit(']')
-        self.failIf(not myLimit.is_sup())
+        my_limit = limit.Limit(']')
+        self.failIf(not my_limit.is_sup())
         pass
     
     def test_if_extreme_included_is_not_inf(self):
-        myLimit = limit.Limit(']')
-        self.failIf(myLimit.is_inf())
+        my_limit = limit.Limit(']')
+        self.failIf(my_limit.is_inf())
         pass
     
     def test_if_is_sup(self):
-        myLimit = limit.Limit(')')
-        self.failIf(not myLimit.is_sup())
+        my_limit = limit.Limit(')')
+        self.failIf(not my_limit.is_sup())
         pass
     
     def test_if_is_not_inf(self):
-        myLimit = limit.Limit(')')
-        self.failIf(myLimit.is_inf())
+        my_limit = limit.Limit(')')
+        self.failIf(my_limit.is_inf())
         pass
     
     def test_if_extreme_is_included(self):
-        myLimit = limit.Limit(')')
-        self.failIf(myLimit.is_extreme_included())
+        my_limit = limit.Limit(')')
+        self.failIf(my_limit.is_extreme_included())
         pass
     
 if __name__ == "__main__":
