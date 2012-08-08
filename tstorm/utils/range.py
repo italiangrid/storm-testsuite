2e6b430c60fd5398d9436c83cb8e8f2df5c04fbc
Elisabetta Ronchieri <elisabetta.ronchieri@cnaf.infn.it>
2012-08-08 11:27:57 +0000

    Fixed indentation

--- tstorm/utils/range.py
+++ tstorm/utils/range.py
@@ -6,19 +6,19 @@ class RangeError(exceptions.Exception):
     pass
 
 class Range:
-    def __init__(self, range):
-        self.sup = limit.Limit(range[len(range)-1])
+    def __init__(self, range_value):
+        self.sup = limit.Limit(range_value[len(range_value)-1])
         if not self.sup.is_sup():
             raise RangeError('Superior Limit is not well specified - %s' % self.sup)
             
-        self.inf = limit.Limit(range[0])
+        self.inf = limit.Limit(range_value[0])
         if not self.inf.is_inf():
             raise RangeError('Inferior Limit is not well specified - %s' % self.inf)
 
-        if ',' not in range:
-            raise RangeError('Range is not well specified - %s' % range)
+        if ',' not in range_value:
+            raise RangeError('Range is not well specified - %s' % range_value)
 
-        extreme = range[1:len(range)-1].strip().split(',')
+        extreme = range_value[1:len(range_value)-1].strip().split(',')
         if len(extreme) == 2:
             self.min_release = release.Release(extreme[0])
             self.max_release = release.Release(extreme[1])
@@ -26,12 +26,12 @@ class Range:
         #    raise RangeError('Range is not well specified - %s' % range)
 
         if not self.min_release.is_infinity():
-           if not self.max_release.is_infinity():
-               if not (self.min_release.is_lower(self.max_release) and \
-                   self.max_release.is_greater(self.min_release)):
-                   msg = 'The couple Min,Max is not well specified in'
-                   msg += 'the range - ' + range
-                   raise RangeError(msg)
+            if not self.max_release.is_infinity():
+                if not (self.min_release.is_lower(self.max_release) and \
+                    self.max_release.is_greater(self.min_release)):
+                    msg = 'The couple Min,Max is not well specified in'
+                    msg += 'the range - ' + range_value
+                    raise RangeError(msg)
 
     def is_included(self, value):
         if self.inf.is_extreme_included():
