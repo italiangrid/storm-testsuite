ad03db457c61f9130d566257dadf5988e4575a67
enricovianello <maverick17586@gmail.com>
2019-12-18 17:02:31 +0100

    fixed BRANCH_TAG init

--- docker/push-image.sh
+++ docker/push-image.sh
@@ -3,8 +3,7 @@ set -ex
 
 if [ -n "${DOCKER_REGISTRY_HOST}" ]; then
 
-    BRANCH_TAG=`git rev-parse --abbrev-ref HEAD`
-    docker tag italiangrid/storm-testsuite:${BRANCH_TAG} ${DOCKER_REGISTRY_HOST}/italiangrid/storm-testsuite:${BRANCH_TAG}
-    docker push ${DOCKER_REGISTRY_HOST}/italiangrid/storm-testsuite:${BRANCH_TAG}
+    docker tag italiangrid/storm-testsuite:${BRANCH_NAME} ${DOCKER_REGISTRY_HOST}/italiangrid/storm-testsuite:${BRANCH_NAME}
+    docker push ${DOCKER_REGISTRY_HOST}/italiangrid/storm-testsuite:${BRANCH_NAME}
 
 fi
