47773a61c8dc3e797240f53498b1ca42cacb0896
enricovianello <maverick17586@gmail.com>
2019-12-18 17:02:31 +0100

    fixed BRANCH_TAG init

--- docker/push-image-dockerhub.sh
+++ docker/push-image-dockerhub.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -ex
 
-BRANCH_TAG=`git rev-parse --abbrev-ref HEAD`
-
-echo "Pushing italiangrid/storm-testsuite:${BRANCH_TAG} on dockerhub ..."
-docker push italiangrid/storm-testsuite:${BRANCH_TAG}
+echo "Pushing italiangrid/storm-testsuite:${BRANCH_NAME} on dockerhub ..."
+docker push italiangrid/storm-testsuite:${BRANCH_NAME}
