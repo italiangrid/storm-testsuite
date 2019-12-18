09c15ece4dd3c82c47445bca8a3a21874cd92535
enricovianello <maverick17586@gmail.com>
2019-12-18 17:02:31 +0100

    fixed BRANCH_TAG init

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -ex
 
-BRANCH_TAG=`git rev-parse --abbrev-ref HEAD`
-docker build --pull=false --rm=true -t italiangrid/storm-testsuite:${BRANCH_TAG} .
+docker build --pull=false --rm=true -t italiangrid/storm-testsuite:${BRANCH_NAME} .
