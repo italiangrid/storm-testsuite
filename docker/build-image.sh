d8752ec02b8c2f3e78c7fa746c5396b5fa342a8c
enricovianello <maverick17586@gmail.com>
2020-09-18 11:38:05 +0200

    Fixed storm-testsuite image

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 set -ex
 
-if [ -z ${BRANCH_NAME+x} ]; then
-  echo "BRANCH_NAME is unset"
-  BRANCH_NAME=`git rev-parse --abbrev-ref HEAD`
-else
-  echo "BRANCH_NAME is set to '$BRANCH_NAME'"
-fi
-
-docker build --no-cache --pull=false --rm=true -t italiangrid/storm-testsuite:${BRANCH_NAME} .
+docker build --pull=false --rm=true -t italiangrid/storm-testsuite .
