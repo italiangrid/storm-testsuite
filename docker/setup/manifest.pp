7547cb4c02e77ce77db9dea2c0b02d7af3b9f093
enricovianello <maverick17586@gmail.com>
2020-09-18 11:38:05 +0200

    Fixed storm-testsuite image

--- docker/setup/manifest.pp
+++ docker/setup/manifest.pp
@@ -1,5 +1,52 @@
-include mwdevel_storm
-include mwdevel_infn_ca
-include mwdevel_terena_ca
-include mwdevel_robot_framework
-include mwdevel_test_vos
+include epel
+include umd4
+include testvos
+include testca
+
+class { 'storm::repo':
+  enabled     => ['stable'],
+}
+
+$packages = [
+  'openldap-clients',
+  'globus-gass-copy-progs',
+  'gfal2-util',
+  'gfal2-all',
+  'dcache-srmclient',
+  'storm-srm-client',
+  'davix',
+  'myproxy',
+]
+
+package { $packages:
+  ensure => 'latest',
+}
+
+package { 'voms-clients':
+  ensure => 'installed',
+  source => 'https://ci.cloud.cnaf.infn.it/view/voms/job/pkg.voms/job/release_sep_19/lastSuccessfulBuild/artifact/repo/centos6/voms-clients3-3.3.1-0.el6.centos.noarch.rpm',
+}
+
+include python
+
+package { 'robotframework':
+  ensure   => installed,
+  require  => Package['pip'],
+  provider => 'pip',
+}
+package { 'robotframework-httplibrary':
+  ensure   => installed,
+  require  => [Package['pip'], Package['robotframework']],
+  provider => 'pip',
+}
+
+
+Class['epel']
+-> Class['umd4']
+-> Class['python']
+-> Class['storm::repo']
+-> Class['storm::testvos']
+-> Class['storm::testca']
+-> Package[$packages]
+-> Package['voms-clients']
+-> Package['robotframework']
