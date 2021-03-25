54846b258c48b1b3821c8458159430ad1b848ae2
enricovianello <maverick17586@gmail.com>
2021-03-25 15:25:44 +0100

    Exit in case of puppet apply errors

--- docker/setup/setup.sh
+++ docker/setup/setup.sh
@@ -3,7 +3,14 @@ set -e
 
 echo 'export X509_USER_PROXY="/tmp/x509up_u$(id -u)"'>/etc/profile.d/x509_user_proxy.sh
 
-puppet apply /setup/manifest.pp
+puppet apply --detailed-exitcodes /setup/manifest.pp
+ec=$?
+if $ec == 4 || $ec == 6; then
+  echo "Puppet apply exited with $ec"
+  exit 1
+else
+
+fi
 
 # Add tester user
 adduser -d /home/tester tester
