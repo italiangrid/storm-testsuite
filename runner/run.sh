7df630aad9933f1af1ace307c1bae8c29eb57c87
Enrico Vianello <vianello@mbp13-vianello.local>
2021-10-15 18:30:17 +0200

    Fixes to default env setup

--- runner/run.sh
+++ runner/run.sh
@@ -2,20 +2,26 @@
 set -ex
 
 outputDir="reports"
-COMPOSE_OPTS="--no-ansi"
-STORM_TESTSUITE_CONTAINER_NAME="${STORM_TESTSUITE_CONTAINER_NAME:-storm-testsuite}"
+COMPOSE_OPTS="--ansi never"
 TTY_OPTS="${TTY_OPTS:-}"
+STORM_TESTSUITE_CONTAINER_NAME="${STORM_TESTSUITE_CONTAINER_NAME:-storm-testsuite}"
+OUTPUT_DIRECTORY="${OUTPUT_DIRECTORY:-output}"
+SKIP_DOCKER_IMAGES_PULL="${SKIP_DOCKER_IMAGES_PULL:-false}"
 
 # Stop if compose is running
 docker-compose ${COMPOSE_OPTS} down
 # Pull images from dockerhub
-docker-compose ${COMPOSE_OPTS} pull
+if [[ ${SKIP_DOCKER_IMAGES_PULL} == "false" ]]
+then
+  docker-compose ${COMPOSE_OPTS} pull
+fi
 
 # Run testsuite
 docker-compose ${COMPOSE_OPTS} up --no-color storm-testsuite
 
 # Copy Reports
-mkdir ${OUTPUT_DIRECTORY}
+mkdir -p ${OUTPUT_DIRECTORY}
+rm -rf ${OUTPUT_DIRECTORY}/*
 docker cp ${STORM_TESTSUITE_CONTAINER_NAME}:/home/tester/storm-testsuite/reports ${OUTPUT_DIRECTORY}/
 
 docker-compose ${COMPOSE_OPTS} rm -f -s trust storm-testsuite
